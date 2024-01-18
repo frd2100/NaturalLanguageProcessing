d4643e9c4b7f69a35617efbcaf8e9db69cec513f
frd2100 <92338767+frd2100@users.noreply.github.com>
2024-01-18 19:14:38 +0100

    add enum types

--- src/main/java/NER/controller/NERController.java
+++ src/main/java/NER/controller/NERController.java
@@ -1,27 +1,39 @@
 package NER.controller;
 
 import NER.model.Type;
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value ="/api")
+@RequestMapping(value = "/api")
 public class NERController {
     @Autowired
     private StanfordCoreNLP stanfordCoreNLP;
 
     @PostMapping
-    @RequestMapping(value  ="/ner")
-    List<String> ner (@RequestBody String input, @RequestParam Type type){
+    @RequestMapping(value = "/ner")
+    Set<String> ner(@RequestBody String input, @RequestParam Type type) {
         CoreDocument coreDocument = new CoreDocument(input);
         stanfordCoreNLP.annotate(coreDocument);
         List<CoreLabel> coreLabels = coreDocument.tokens();
 
-        return null;
+        return new HashSet<>(collectList(coreLabels, type));
+    }
+
+    private List<String> collectList(List<CoreLabel> coreLabels, final Type type) {
+        return coreLabels
+                .stream()
+                .filter(coreLabel -> type.getName().equalsIgnoreCase(coreLabel.get(CoreAnnotations.NamedEntityTagAnnotation.class)))
+                .map(CoreLabel::originalText)
+                .collect(Collectors.toList());
     }
 }
